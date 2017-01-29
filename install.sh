80c77fa7b04c08f8c3b39a377bdad9f233f0080c
skmpz <dem.procopiou@gmail.com>
2017-01-29 21:03:41 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
+# package installation
+sudo pacman -S --noconfirm git i3-wm i3lock i3status alsa-utils alsa-oss xorg xterm xorg-xclock xorg-twm xorg-xinit xorg-server-utils
+
 # NeoVIM
 #mkdir -p $HOME/.config/nvim/
 #ln -s $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
 
 # xinitrc
-MON_1=`xrandr | grep " connected" | awk '{ print $1 }' | head -1`
-MON_2=`xrandr | grep " connected" | awk '{ print $1 }' | tail -1`
-echo "xrandr --output $MON_2 --auto --left-of $MON_1" | cat - .xinitrc > .tmptmp && mv .tmptmp ~/.xinitrc
+#MON_1=`xrandr | grep " connected" | awk '{ print $1 }' | head -1`
+#MON_2=`xrandr | grep " connected" | awk '{ print $1 }' | tail -1`
+#echo "xrandr --output $MON_2 --auto --left-of $MON_1" | cat - .xinitrc > .tmptmp && mv .tmptmp ~/.xinitrc
 
 # Xresources
-sudo pacman -S xcursor-themes
-ln -s $HOME/dotfiles/.Xresources $HOME/.Xresources
+#sudo pacman -S xcursor-themes
+#ln -s $HOME/dotfiles/.Xresources $HOME/.Xresources
 
 # i3 blocks
 #wget https://aur.archlinux.org/cgit/aur.git/snapshot/i3blocks.tar.gz
