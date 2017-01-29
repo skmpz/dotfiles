3469bae9e0d81e286bd3b6182d5b680426626798
skmpz <dem.procopiou@gmail.com>
2017-01-29 21:34:46 +0200

    i3

--- install.sh
+++ install.sh
@@ -23,6 +23,8 @@ tar xf i3blocks.tar.gz
 cd i3blocks
 makepkg -sri --skippgpcheck --noconfirm
 cp -r $HOME/dotfiles/i3/ $HOME/.config/i3/
+cd ..
+rm -rf i3blocks*
 mkdir -p $HOME/.local/share/fonts
 cp fonts/*.ttf $HOME/.local/share/fonts
 fc-cache -fv
