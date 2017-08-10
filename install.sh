1b725adc79e090a107dadf14e637e0e1c61b5f4b
Demetris Procopiou <dem.procopiou@gmail.com>
2017-08-10 19:02:16 +0300

    update

--- install.sh
+++ install.sh
@@ -13,8 +13,9 @@ function check {
 sudo ls > /dev/null
 
 echo -n "Setting up mirrors........... "
-sudo pacman -S reflector --noconfirm > /dev/null 2>&1
-sudo reflector --verbose -l 5 --sort rate --save /etc/pacman.d/mirrorlist > /dev/null 2>&1
+sudo pacman -Syy > /dev/null 2> .install.log
+sudo pacman -S reflector --noconfirm > /dev/null 2> .install.log
+sudo reflector --verbose -l 5 --sort rate --save /etc/pacman.d/mirrorlist > /dev/null 2> .install.log
 check $?
 
 echo -n "Installing system tools...... "
@@ -22,31 +23,31 @@ sudo pacman -S alsa-utils alsa-oss openssh bash-completion bc wget tmux cmake py
 check $?
 
 echo -n "Install X window manager..... "
-sudo pacman -S xorg xterm xorg-xclock xorg-twm xorg-xinit polkit xcursor-themes xclip --noconfirm --needed > /dev/null 2>&1
+sudo pacman -S xorg xterm xorg-xclock xorg-twm xorg-xinit polkit xcursor-themes xclip --noconfirm --needed > /dev/null 2> .install.log
 check $?
 
 echo -n "Installing i3 and wm tools... "
-sudo pacman -S i3-wm i3lock i3blocks i3status rofi rxvt-unicode feh arc-icon-theme arc-gtk-theme ttf-dejavu terminus-font adobe-source-code-pro-fonts ttf-droid ttf-inconsolata --noconfirm --needed > /dev/null 2>&1
+sudo pacman -S i3-wm i3lock i3blocks i3status rofi rxvt-unicode feh arc-icon-theme arc-gtk-theme ttf-dejavu terminus-font adobe-source-code-pro-fonts ttf-droid ttf-inconsolata --noconfirm --needed > /dev/null 2> .install.log
 check $?
 
 echo -n "Installing applications...... "
-sudo pacman -S engrampa evince virtualbox-host-modules-arch caja neovim chromium synergy qt4 qt vlc transmission-gtk --noconfirm --needed > /dev/null 2>&1
+sudo pacman -S engrampa evince virtualbox-host-modules-arch caja neovim chromium synergy qt4 qt vlc transmission-gtk --noconfirm --needed > /dev/null 2> .install.log
 check $?
 
 echo -n "Setting up i3-gaps........... "
-git clone https://www.github.com/Airblader/i3 i3-gaps > /dev/null 2>&1
+git clone https://www.github.com/Airblader/i3 i3-gaps > /dev/null 2> .install.log
 cd i3-gaps
-autoreconf --force --install > /dev/null 2>&1
+autoreconf --force --install > /dev/null 2> .install.log
 rm -rf build/
 mkdir -p build && cd build/
-../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers > /dev/null 2>&1
-make > /dev/null 2>&1
-sudo make install > /dev/null 2>&1
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers > /dev/null 2> .install.log
+make > /dev/null 2> .install.log
+sudo make install > /dev/null 2> .install.log
 cd ../../
 check $?
 
 echo -n "Setting up neovim............ "
-sudo pip3 install neovim > /dev/null 2>&1
+sudo pip3 install neovim > /dev/null 2> .install.log
 check $?
 
 echo -n "Setting up config files.. "
@@ -62,8 +63,8 @@ rm -rf $HOME/.config/i3/*
 rm -rf $HOME/.config/gtk-3.0/settings.ini
 rm -rf $HOME/.config/nvim/init.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1
-git clone https://github.com/skmpz/git-aware-prompt $HOME/.bash/git-aware-prompt > /dev/null 2>&1
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2> .install.log
+git clone https://github.com/skmpz/git-aware-prompt $HOME/.bash/git-aware-prompt > /dev/null 2> .install.log
 ln -s $HOME/dotfiles/bashrc $HOME/.bashrc
 ln -s /home/sk/dotfiles/gtkrc-2.0 $HOME/.gtkrc-2.0
 ln -s $HOME/dotfiles/xinitrc $HOME/.xinitrc
@@ -78,8 +79,8 @@ ln -s $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
 ln -s $HOME/dotfiles/settings.ini $HOME/.config/gtk-3.0/settings.ini
 mkdir ~/.local/share/fonts
 cp $HOME/dotfiles/fonts/fontawesome-webfont.ttf ~/.local/share/fonts/
-fc-cache -fv > /dev/null 2>&1
+fc-cache -fv > /dev/null 2> .install.log
 nvim +PlugInstall +qall > /dev/null
 nvim +UpdateRemotePlugins +qall > /dev/null
-sudo ntpdate time.nist.gov > /dev/null 2>&1
+sudo ntpdate time.nist.gov > /dev/null 2> .install.log
 check $?
