f8f566db5e4fa4bc1999b71c9076d22b1aebd083
skmpz <dem.procopiou@gmail.com>
2018-03-20 22:33:17 +0200

    change

---
+++ freebsd/bin/startup.sh
@@ -0,0 +1,69 @@
+#!/usr/local/bin/bash
+
+i3-msg 'workspace main [1]; exec urxvt -e bash -c "nvim /data/other/TODO"'
+sleep 0.5
+i3-msg 'workspace main [1]; exec chrome "http://www.gmail.com"'
+sleep 10
+i3-msg 'workspace main [1]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace main [1]; split v'
+sleep 0.5
+i3-msg 'workspace main [1]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace main [1]; focus left;'
+sleep 0.5
+i3-msg 'workspace main [1]; resize grow width 18 px or 18 ppt;'
+sleep 0.5
+i3-msg 'workspace main [1]; focus left;'
+sleep 0.5
+i3-msg 'workspace main [1]; split v'
+sleep 0.5
+i3-msg 'workspace main [1]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace cmd [2]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace cmd [2]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace cmd [2]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace cmd [2]; split v'
+sleep 0.5
+i3-msg 'workspace cmd [2]; exec urxvt -e bash -c "top && bash"'
+sleep 0.5
+i3-msg 'workspace cmd [2]; focus left;'
+sleep 0.5
+i3-msg 'workspace cmd [2]; resize grow width 14 px or 14 ppt;'
+sleep 0.5
+i3-msg 'workspace cmd [2]; focus left;'
+sleep 0.5
+i3-msg 'workspace cmd [2]; split v'
+sleep 0.5
+i3-msg 'workspace cmd [2]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace apps [3]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace apps [3]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace apps [3]; exec urxvt -e bash -c "rtorrent"'
+sleep 0.5
+i3-msg 'workspace apps [3]; split v'
+sleep 0.5
+i3-msg 'workspace apps [3]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace apps [3]; focus left'
+sleep 0.5
+i3-msg 'workspace apps [3]; split v'
+sleep 0.5
+i3-msg 'workspace apps [3]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace apps [3]; focus left'
+sleep 0.5
+i3-msg 'workspace apps [3]; split v'
+sleep 0.5
+i3-msg 'workspace apps [3]; exec urxvt'
+sleep 0.5
+i3-msg 'workspace books [4]; exec calibre'
+sleep 5
+i3-msg 'workspace cmd [2]'
+sleep 0.5
+i3-msg 'workspace main [1]'
