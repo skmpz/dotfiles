75f29aecf104da114253a6554a5e37f42dcb9d5f
skmpz <dem.procopiou@gmail.com>
2018-06-20 21:49:41 +0300

    changes

--- freebsd/bin/scrot.sh
+++ freebsd/bin/scrot.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
 
-TARGET=$(ssh kali cat TARGET)
+TARGET=$(ssh oscp cat TARGET)
 mkdir /data/shared/notes/$TARGET/screenshots/
 scrot -s /data/shared/notes/$TARGET/screenshots/%d-%m-%Y-%H-%M-%S.png -q 100
---
+++ freebsd/bin/scrot_report.sh
@@ -0,0 +1,3 @@
+#!/usr/local/bin/bash
+
+scrot -s /data/shared/screen/%d-%m-%Y-%H-%M-%S.png -q 100
