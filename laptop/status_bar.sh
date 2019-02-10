f1fb795e4f9621abdb71c7c3c50dbf95b5e7f19e
sk <dem.procopiou@gmail.com>
2019-02-11 00:22:34 +0200

    changes

--- laptop/status_bar.sh
+++ laptop/status_bar.sh
@@ -8,11 +8,17 @@ do
 
     VERSION=$(uname -r)
 
-    MUTE=$(pactl list sinks | grep -A8 'Sink #1' | grep Mute | awk '{print $2}')
+    MUTE=$(pactl list sinks | grep -A8 'Sink #0' | grep Mute | awk '{print $2}')
     if [ "$MUTE" == "no" ]; then
-	VOL=$(pactl list sinks | grep Volume | grep -v Base | tail -1 | awk '{print $5}')
-    else
-        VOL="MUTE"
+        VOL_PERC=$(pactl list sinks | grep Volume | grep -v Base | tail -1 | awk '{print $5}')
+        VOL_NUM=$(echo $VOL_PERC | tr -d '%')
+        if [ "$VOL_NUM" -ge 0 ] && [ "$VOL_NUM" -lt 10 ]; then
+            VOL=" $VOL_PERC"
+        elif [ "$VOL_NUM" -ge 5 ] && [ "$VOL_NUM" -lt 55 ]; then
+            VOL=" $VOL_PERC"
+        else
+            VOL=" $VOL_PERC"
+        fi
     fi
     DATE=$(date +'%A %d/%m/%Y')
     TIME=$(date +'%H:%M')
@@ -20,13 +26,23 @@ do
     H_USED=$(df -h / | tail -1 | awk '{print $3}')
     H_TOTAL=$(df -h /home/ | tail -1 | awk '{print $2}')
     H_PERC=$(df -h /home/ | tail -1 | awk '{print $5}')
-    D_USED=$(df -h /data/ | tail -1 | awk '{print $3}')
-    D_TOTAL=$(df -h /data/ | tail -1 | awk '{print $2}')
-    D_PERC=$(df -h /data/ | tail -1 | awk '{print $5}')
     MEM_USED=$(free -mh | grep Mem | awk '{print $3}')
     MEM_TOTAL=$(free -mh | grep Mem | awk '{print $2}')
-    IP=$(ip a | grep -A3 enp6s0 | grep -w inet | awk '{print $2}' | cut -f1 -d'/')
-    full=" $VERSION |  $H_USED/$H_TOTAL [$H_PERC] |  $D_USED/$D_TOTAL [$D_PERC] |  $MEM_USED/$MEM_TOTAL |  $IP |  $uptime |  $LOAD |  $VOL |  $DATE |  $TIME ";
+    IP=$(ip a | grep -A3 wlo1 | grep -w inet | awk '{print $2}' | cut -f1 -d'/')
+    BATTERY_PERC=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep perc | awk '{print $2}')
+    BATTERY_NUM=$(echo $BATTERY_PERC | tr -d '%')
+    if [ "$BATTERY_NUM" -ge 0 ] && [ "$BATTERY_NUM" -lt 5 ]; then
+        BATTERY=" $BATTERY_PERC"
+    elif [ "$BATTERY_NUM" -ge 5 ] && [ "$BATTERY_NUM" -lt 30 ]; then
+        BATTERY=" $BATTERY_PERC"
+    elif [ "$BATTERY_NUM" -ge 30 ] && [ "$BATTERY_NUM" -lt 60 ]; then
+        BATTERY=" $BATTERY_PERC"
+    elif [ "$BATTERY_NUM" -ge 60 ] && [ "$BATTERY_NUM" -lt 85 ]; then
+        BATTERY=" $BATTERY_PERC"
+    else
+        BATTERY=" $BATTERY_PERC"
+    fi
+    full=" $VERSION |  $H_USED/$H_TOTAL [$H_PERC] |  $MEM_USED/$MEM_TOTAL |  $IP |  $uptime |  $LOAD | $VOL | $BATTERY |  $DATE |  $TIME ";
     echo "$full"
     sleep 1
 done
