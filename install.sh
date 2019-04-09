68cab6d5957b056ca719a9dcabcd4c80f9fbaa96
sk <dem.procopiou@gmail.com>
2019-04-09 14:43:45 +0300

    changes

--- install.sh
+++ install.sh
@@ -116,9 +116,8 @@ _start "Updating"
 sudo apt update -y >> $LOGFILE 2>&1 && sudo apt upgrade -y >> $LOGFILE 2>&1
 _check_ok $?
 
-exit 0
 _start "Installing system"
-sudo apt install -y aircrack-ng alsa-oss alsa-utils alsa-utils arc-theme bash-completion bc binwalk caja caja-open-terminal calibre chromium-browser cherrytree clang cmake engrampa evince fbreader feh foremost gdb gedit gparted i3lock i3status imagemagick libimage-exiftool-perl openjdk-8-jdk ltrace luarocks mate-terminal mpv neovim nodejs nomacs fonts-font-awesome p7zip pulseaudio python-neovim python python3 rofi rsync ruby rxvt-unicode scala scrot strace synergy tmux transmission-gtk ttf-dejavu fonts-inconsolata ttf-ubuntu-font-family unrar upower vim wget wireshark-gtk xclip xcursor-themes xorg xterm >> $LOGFILE 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt install -yq aircrack-ng alsa-oss alsa-utils alsa-utils arc-theme bash-completion bc binwalk caja caja-open-terminal calibre chromium-browser cherrytree clang cmake engrampa evince fbreader feh foremost gdb gedit gparted i3lock i3status imagemagick libimage-exiftool-perl openjdk-8-jdk ltrace luarocks mate-terminal mpv neovim nodejs nomacs fonts-font-awesome p7zip pulseaudio python-neovim python python3 rofi rsync ruby rxvt-unicode scala scrot strace synergy tmux transmission-gtk ttf-dejavu fonts-inconsolata ttf-ubuntu-font-family unrar upower vim wget wireshark-gtk xclip xcursor-themes xorg xterm >> $LOGFILE 2>&1
 _check_ok $?
 
 _start "Installing extra tools"
