8dbdfcff2479fc8a07619774a99e259c019b0a7f
sk <dem.procopiou@gmail.com>
2019-04-09 14:24:37 +0300

    changes

--- install.sh
+++ install.sh
@@ -108,13 +108,15 @@ if [ "$mode" != "HOME" ] && [ "$mode" != "VM" ] && [ "$mode" != "LAPTOP" ] && [ 
 # ------------------------- arguments --------------------------
 
 _start "Setting passwordless sudo"
-echo "%wheel ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers >> $LOGFILE 2>&1
+echo "sk ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers >> $LOGFILE 2>&1
 _check_ok $?
 
-_start "Getting pacman up to date"
-sudo pacman -Syy --noconfirm --needed >> $LOGFILE 2>&1
+_start "Updating"
+# sudo pacman -Syy --noconfirm --needed >> $LOGFILE 2>&1
+sudo apt update -y >> $LOGFILE 2>&1 && sudo apt upgrade -y >> $LOGFILE 2>&1
 _check_ok $?
 
+exit 0
 _start "Installing system"
 sudo pacman -S adobe-source-code-pro-fonts aircrack-ng alsa-oss alsa-utils alsa-utils arc-gtk-theme arc-icon-theme bash-completion bc binwalk caja caja-open-terminal calibre checksec cherrytree chromium clang cmake engrampa evince fbreader feh fzf foremost gdb gedit gparted i3-gaps i3lock i3status imagemagick jdk8-openjdk ltrace luarocks mate-terminal mpv neovim nodejs nomacs openssh otf-font-awesome p7zip perl-image-exiftool polkit pulseaudio python-neovim python2 python3 rofi rsync ruby rxvt-unicode sbt scala scrot strace synergy tmux transmission-gtk ttf-dejavu ttf-droid ttf-inconsolata ttf-ubuntu-font-family unrar upower urxvt-perls vim wget wireshark-gtk xclip xcursor-themes xorg xorg-twm xorg-xclock xorg-xinit xterm --noconfirm --needed >> $LOGFILE 2>&1
 _check_ok $?
