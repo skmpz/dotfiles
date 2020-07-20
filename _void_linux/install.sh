7a042175489a122f579a6d7bba3fe0a08128c540
sk <dem.procopiou@gmail.com>
2020-07-20 20:33:51 +0400

    changes

--- _void_linux/install.sh
+++ _void_linux/install.sh
@@ -1,90 +1,12 @@
 #!/bin/bash
 
-# ------------------------- settings ---------------------------
-s_log_cols=50           # log length
-s_line_cols=80          # hr line length
-s_sudo_perm="YES"       # sudo perm needed
-s_line_color="BLUE"     # hr line color
-s_main_color="YELLOW"   # main action color
-s_plus_color="WHITE"    # sign color
-s_ok_color="GREEN"      # ok color
-s_info_color="PURPLE"   # info color
-s_fail_color="RED"      # fail color
-# ------------------------- settings ---------------------------
-
-# ----------------------- helper funcs -------------------------
-# colors
-NC="\033[0m"        RED="\033[0;31m"   GREEN="\033[0;32m"
-YELLOW="\033[0;33m" WHITE="\033[0;97m" BLUE="\033[0;34m"
-PURPLE="\033[0;35m" CYAN="\033[0;36m"  GREY="\033[0;90m"
-
-# check function (with ok output)
-function _check_ok {
-    if [ $1 == 0 ]; then
-        echo -en "[${!s_ok_color}OK${NC}]"
-        if [ ! -z "$2" ]; then echo -e "[${!s_info_color}${2}${NC}]"; else echo ""; fi
-    else
-        echo -e "[${!s_fail_color}FAIL${NC}]"; exit 1
-    fi
-}
-
-# check function (without ok output)
-function _check_no_ok {
-    if [ $1 == 0 ]; then return; else echo -e "[${!s_fail_color}FAIL${NC}]"; exit 1; fi
-}
-
-# section start
-function _start {
-    str_len=${#1}
-    str_len=$((s_log_cols-str_len))
-    echo -en "${!s_main_color}[${!s_plus_color}+${!s_main_color}] $1 "
-    v=$(printf "%-${str_len}s" ".")
-    echo -en "${v// /.} ${NC}"
-    echo "------- $1" >> $LOGFILE
-}
-
-# no check ok
-function _ok {
-    echo -en "[${!s_ok_color}OK${NC}]"
-    if [ ! -z "$1" ]; then echo -e "[${PURPLE}${1}${NC}]"; else echo ""; fi
-}
-
-# no check fail
-function _fail {
-    echo -en "[${!s_fail_color}FAIL${NC}]"
-    if [ ! -z "$1" ]; then echo -e "[${PURPLE}${1}${NC}]"; else echo ""; fi
-    exit 1
-}
-
-# hr line
-function _line {
-    v=$(printf "%-${s_line_cols}s" "-")
-    echo -e "${!s_line_color}${v// /-} ${NC}"
-}
-
-# sudo perm
-function _sudo {
-    sudo ls > /dev/null
-}
-# ----------------------- helper funcs -------------------------
-
-# ------------------------ initialize --------------------------
-CWD=$PWD
-LOGFILE="$CWD/.${0//.\/}.log"   # set logfile
-echo "" > $LOGFILE              # empty logfile
-start_time=`date +%s`           # start timer
-if [ "$s_sudo_perm" == "YES" ]; then _sudo; fi
-_line
-# ------------------------ initialize --------------------------
-
-# ------------------------- arguments --------------------------
 # print usage and exit
 function show_usage {
-    echo -e "[${RED}usage${NC}] ${!s_main_color}./$(basename $0) [opts]${NC}"
-    _line
-    echo -en "[${WHITE}opts${NC}] "
-    echo -e "${BLUE}laptop/home/vm    install mode    [required]${NC}"
-    _line
+    echo "[usage] ./$(basename $0) [opts]"
+    echo "-----------------------------------------------"
+    echo -n "[opts] "
+    echo "laptop/home/vm    install mode    [required]"
+    echo "-----------------------------------------------"
     exit 1
 }
 
@@ -106,7 +28,7 @@ done
 if [ "$mode" != "HOME" ] && [ "$mode" != "VM" ] && [ "$mode" != "LAPTOP" ]; then show_usage; fi
 # ------------------------- arguments --------------------------
 
-_start "Installing system"
+echo "Installing system.."
 sudo xbps-install -Sy \
 alsa-oss \
 alsa-utils \
@@ -164,31 +86,6 @@ xclip \
 xcursor-themes \
 xorg \
 xterm \
->> $LOGFILE 2>&1
-_check_ok $?
-
-    # _start "Installing extra tools"
-    # rm -rf i3-gaps cquery
-    # git clone https://www.github.com/Airblader/i3 i3-gaps >> $LOGFILE 2>&1
-    # cd i3-gaps >> $LOGFILE 2>&1
-    # git checkout gaps  >> $LOGFILE 2>&1
-    # git pull >> $LOGFILE 2>&1
-    # autoreconf --force --install >> $LOGFILE 2>&1
-    # rm -rf build >> $LOGFILE 2>&1
-    # mkdir build >> $LOGFILE 2>&1
-    # cd build >> $LOGFILE 2>&1
-    # ../configure --prefix=/usr --sysconfdir=/etc >> $LOGFILE 2>&1
-    # make -j4 >> $LOGFILE 2>&1
-    # sudo make install >> $LOGFILE 2>&1
-    # cd ../..
-    # git clone --recursive https://github.com/cquery-project/cquery.git >> $LOGFILE 2>&1
-    # cd cquery
-    # git submodule update --init >> $LOGFILE 2>&1
-    # mkdir build && cd build
-    # cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES >> $LOGFILE 2>&1
-    # cmake --build . >> $LOGFILE 2>&1
-    # cmake --build . --target install >> $LOGFILE 2>&1
-    # _check_ok $?
 
 # # yay -S hopper burpsuite python2-pwntools --noconfirm >> $LOGFILE 2>&1
 # # _check_no_ok $?
