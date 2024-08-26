979838780397714903abf6bbe4940d4ec6b82685
skmpz <dem.procopiou@gmail.com>
2024-08-26 11:05:45 +0300

    changes

--- sway/start.sh
+++ sway/start.sh
@@ -1,4 +1,5 @@
 cat $HOME/.config/sway/config.base $HOME/.config/sway/config.local > $HOME/.config/sway/config
 export _JAVA_AWT_WM_NONREPARENTING=1
-export XDG_CURRENT_DESKTOP=sway
+export XDG_SESSION_TYPE=wayland
+export XDG_CURRENT_DESKTOP="sway"
 dbus-run-session ssh-agent sway
