9f3b1cbaaf005d4387f95f179f238936f7c33438
skmpz <dem.procopiou@gmail.com>
2024-11-20 14:35:14 +0200

    changes

--- nvim/lua/sk/plugins/lualine.lua
+++ nvim/lua/sk/plugins/lualine.lua
@@ -64,6 +64,7 @@ return {
                         cond = lazy_status.has_updates,
                         color = { fg = "#ff9e64" },
                     },
+                    { "hostname" },
                     { "encoding" },
                     { "fileformat" },
                     { "filetype" },
