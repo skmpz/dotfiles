1209cb6e8fabd9734aed32fb6de1879c3f44d631
skmpz <dem.procopiou@gmail.com>
2025-04-11 19:18:30 +0300

    changes

--- _void_linux/zen/startup.sh
+++ _void_linux/zen/startup.sh
@@ -1,67 +1,78 @@
 #!/bin/bash
 
-middle_display=$(swaymsg -t get_outputs -p | grep FVVS5H3 | cut -f2 -d' ')
+# fix audio
+~/dotfiles/sway/fixaudio.sh
 
-if [ "$middle_display" == "DP-4" ]; then
-    swaymsg 'workspace 1; workspace 7;'
+monitor_count=$(swaymsg -t get_outputs -p | grep Output | wc -l)
+
+if [ $monitor_count == "2" ]; then
+    swaymsg 'workspace 1; exec alacritty'
+else
+
+    # 3-screen setup
+    middle_display=$(swaymsg -t get_outputs -p | grep 6PB0P44 | cut -f2 -d' ')
+    left_display=$(swaymsg -t get_outputs -p | grep DP9Q9V3 | cut -f2 -d' ')
+    right_display="HDMI-A-1"
+    laptop_display="eDP-1"
+
+    # reset
+    swaymsg 'workspace 1; workspace 5'
+    swaymsg 'workspace 2; workspace 6'
+    swaymsg 'workspace 3; workspace 7'
+    swaymsg 'workspace 4; workspace 8'
+
+    # workspace 1
+    swaymsg 'workspace 1; exec alacritty'
+    sleep 1
+    swaymsg 'workspace 1; split v'
+    sleep 1
+    swaymsg 'workspace 1; exec google-chrome-stable'
+    sleep 8
+    swaymsg 'workspace 1; resize set height 3500px'
+    sleep 1
+    swaymsg 'workspace 1; focus up;'
+    sleep 1
+    swaymsg 'workspace 1; split h'
+    sleep 1
+    swaymsg 'workspace 1; exec alacritty'
+    sleep 1
+
+    # workspace 2
+    swaymsg 'workspace 2; exec alacritty'
+    sleep 1
+    swaymsg 'workspace 2; split v'
+    sleep 1
+    swaymsg 'workspace 2; exec joplin --enable-features=UseOzonePlatform --ozone-platform=wayland'
+    sleep 8
+    swaymsg 'workspace 2; resize set height 3500px'
+    sleep 1
+    swaymsg 'workspace 2; focus up;'
+    sleep 1
+    swaymsg 'workspace 2; split h'
+    sleep 1
+    swaymsg 'workspace 2; exec alacritty'
+    sleep 1
+
+    # workspace 3
+    swaymsg 'workspace 3; exec alacritty'
+    sleep 1
+    swaymsg 'workspace 3; exec alacritty'
+    sleep 1
+
+    # # workspace 4
+    swaymsg 'workspace 4; exec alacritty'
+    sleep 1
+    swaymsg 'workspace 4; exec alacritty'
+    sleep 1
+
+    # # workspace 9
+    swaymsg 'workspace 9; exec keepassxc'
+    sleep 3
+
+    # set current to main
+    swaymsg 'workspace 1'
+    sleep 1
+    swaymsg 'workspace 2'
+    sleep 1
+    swaymsg 'workspace 3'
 fi
-
-# workspace 1
-swaymsg 'workspace 1; exec alacritty'
-sleep 1
-swaymsg 'workspace 1; split v'
-sleep 1
-swaymsg 'workspace 1; exec google-chrome-stable'
-sleep 8
-swaymsg 'workspace 1; resize set height 3500px'
-sleep 1
-swaymsg 'workspace 1; focus up;'
-sleep 1
-swaymsg 'workspace 1; split h'
-sleep 1
-swaymsg 'workspace 1; exec alacritty'
-sleep 1
-
-# workspace 2
-swaymsg 'workspace 2; exec alacritty'
-sleep 1
-swaymsg 'workspace 2; split v'
-sleep 1
-swaymsg 'workspace 2; exec joplin --enable-features=UseOzonePlatform --ozone-platform=wayland'
-sleep 8
-swaymsg 'workspace 2; resize set height 3500px'
-sleep 1
-swaymsg 'workspace 2; focus up;'
-sleep 1
-swaymsg 'workspace 2; split h'
-sleep 1
-swaymsg 'workspace 2; exec alacritty'
-sleep 1
-swaymsg 'workspace 8; exec alacritty'
-sleep 1
-swaymsg 'workspace 8; split v'
-sleep 1
-swaymsg 'workspace 8; exec alacritty -o font.size=14 --hold -e weechat'
-
-# workspace 3
-swaymsg 'workspace 3; exec alacritty'
-sleep 1
-swaymsg 'workspace 3; exec alacritty'
-sleep 1
-
-# # workspace 4
-swaymsg 'workspace 4; exec alacritty'
-sleep 1
-swaymsg 'workspace 4; exec alacritty'
-sleep 1
-
-# # workspace 9
-swaymsg 'workspace 9; exec keepassxc'
-sleep 3
-
-# set current to main
-swaymsg 'workspace 1'
-sleep 1
-swaymsg 'workspace 2'
-sleep 1
-swaymsg 'workspace 3'
--- _void_linux/zen/sway/layout_select.sh
+++ _void_linux/zen/sway/layout_select.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Get connected outputs
-connected_outputs=$(swaymsg -t get_outputs | jq -r '.[] | select(.active == true) | .model')
+monitor_count=$(swaymsg -t get_outputs -p | grep Output | wc -l)
 
-# Define your monitor setup
-if echo "$connected_outputs" | grep -q "U2720Q"; then
+if [ $monitor_count == "2" ]; then
     # 1-screen setup
     swaymsg output eDP-1 pos 4952 2550
     swaymsg output HDMI-A-1 pos 4005 390
@@ -16,13 +14,17 @@ if echo "$connected_outputs" | grep -q "U2720Q"; then
     swaymsg workspace 9 output eDP-1
 else
     # 3-screen setup
-    swaymsg output eDP-1 pos 4823 4436
-    swaymsg output DP-4 pos 3840 2276
-    swaymsg output DP-2 pos 1680 1976 transform 270
-    swaymsg output HDMI-A-1 pos 7680 1958 transform 90
-    swaymsg workspace 1 output DP-2
-    swaymsg workspace 2 output HDMI-A-1
-    swaymsg workspace 3 output DP-4
-    swaymsg workspace 4 output DP-4
-    swaymsg workspace 9 output eDP-1
+    middle_display=$(swaymsg -t get_outputs -p | grep 6PB0P44 | cut -f2 -d' ')
+    left_display=$(swaymsg -t get_outputs -p | grep DP9Q9V3 | cut -f2 -d' ')
+    right_display="HDMI-A-1"
+    laptop_display="eDP-1"
+    swaymsg output ${laptop_display} pos 4823 4436
+    swaymsg output ${middle_display} pos 3840 2276
+    swaymsg output ${left_display} pos 1680 1976 transform 270
+    swaymsg output ${right_display} pos 7680 1958 transform 90
+    swaymsg workspace 1 output ${left_display}
+    swaymsg workspace 2 output ${right_display}
+    swaymsg workspace 3 output ${middle_display}
+    swaymsg workspace 4 output ${middle_display}
+    swaymsg workspace 9 output ${laptop_display}
 fi
