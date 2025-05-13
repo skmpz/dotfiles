d6e21a58bf2b0a937c004285b710c405104ae9bc
skmpz <dem.procopiou@gmail.com>
2025-05-13 09:52:33 +0300

    changes

--- _void_linux/siri/sway/layout_select.sh
+++ _void_linux/siri/sway/layout_select.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# Get connected outputs
-connected_outputs=$(swaymsg -t get_outputs | jq -r '.[] | select(.active == true) | .model')
+monitor_count=$(swaymsg -t get_outputs -p | grep Output | wc -l)
 
-# Define your monitor setup
-if echo "$connected_outputs" | grep -q "U2720Q"; then
+if [ $monitor_count == "2" ]; then
     # 1-screen setup
-    swaymsg output HDMI-A-1 pos 4005 390
-    swaymsg workspace 1 output HDMI-A-1
-    swaymsg workspace 2 output HDMI-A-1
-    swaymsg workspace 3 output HDMI-A-1
-    swaymsg workspace 4 output HDMI-A-1
-    swaymsg workspace 5 output HDMI-A-1
-    swaymsg workspace 9 output eDP-2
+    middle_display=$(swaymsg -t get_outputs -p | grep Output | grep -v eDP | cut -f2 -d' ')
+    laptop_display=$(swaymsg -t get_outputs -p | grep Output | grep eDP | cut -f2 -d' ')
+    swaymsg output ${laptop_display} pos 4952 2550
+    swaymsg output ${middle_display} pos 4005 390
+    swaymsg workspace 1 output ${middle_display}
+    swaymsg workspace 2 output ${middle_display}
+    swaymsg workspace 3 output ${middle_display}
+    swaymsg workspace 4 output ${middle_display}
+    swaymsg workspace 5 output ${middle_display}
+    swaymsg workspace 9 output ${laptop_display}
 else
     # 3-screen setup
-    output DP-6 pos 3980 390
-    output DP-7 pos 1820 0 transform 270
-    output HDMI-A-1 pos 7820 0 transform 90
-    workspace 1 output DP-7
-    workspace 2 output HDMI-A-1
-    workspace 3 output DP-6
-    workspace 4 output DP-6
-    workspace 9 output eDP-2
+    middle_display=$(swaymsg -t get_outputs -p | grep 6PB0P44 | cut -f2 -d' ')
+    left_display=$(swaymsg -t get_outputs -p | grep DP9Q9V3 | cut -f2 -d' ')
+    right_display=$(swaymsg -t get_outputs -p | grep DP9P9V3 | cut -f2 -d' ')
+    laptop_display=$(swaymsg -t get_outputs -p | grep Output | grep eDP | cut -f2 -d' ')
+    swaymsg output ${laptop_display} pos 4823 4436
+    swaymsg output ${middle_display} pos 3840 2276
+    swaymsg output ${left_display} pos 1680 1976 transform 270
+    swaymsg output ${right_display} pos 7680 1958 transform 90
+    swaymsg workspace 1 output ${left_display}
+    swaymsg workspace 2 output ${right_display}
+    swaymsg workspace 3 output ${middle_display}
+    swaymsg workspace 4 output ${middle_display}
+    swaymsg workspace 9 output ${laptop_display}
 fi
