fa104fefffb182fa9e812e5181d777999532da2a
skmpz <dem.procopiou@gmail.com>
2025-12-15 07:59:11 +0200

    changes

--- _void_linux/pad/alacritty/alacritty-start.sh
+++ _void_linux/pad/alacritty/alacritty-start.sh
@@ -4,7 +4,7 @@
 output=$(swaymsg -t get_workspaces | jq -r '.[] | select(.focused).output')
 
 if [ "$output" = "eDP-1" ]; then
-    alacritty --config-file ~/dotfiles/_void_linux/envie/alacritty/alacritty.toml
+    alacritty --config-file ~/dotfiles/_void_linux/pad/alacritty/alacritty.toml
 else
-    alacritty --config-file ~/dotfiles/_void_linux/envie/alacritty/alacritty-smaller.toml
+    alacritty --config-file ~/dotfiles/_void_linux/pad/alacritty/alacritty-smaller.toml
 fi
