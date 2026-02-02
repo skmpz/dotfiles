5d9573183ef8996d64cdf4593dde748b4ea792f8
skmpz <dem.procopiou@gmail.com>
2026-02-02 21:31:52 +0200

    changes

--- sway/startup.sh
+++ sway/startup.sh
@@ -12,21 +12,21 @@ monitor_count=$(swaymsg -t get_outputs -p | grep Output | wc -l)
 if [ $monitor_count == "1" ] || [ $monitor_count == "2" ]; then
     swaymsg 'workspace 1; exec joplin --enable-features=UseOzonePlatform --ozone-platform=wayland' && sleep 8
     swaymsg 'workspace 2; exec google-chrome-stable' && sleep 8
-    swaymsg 'workspace 3; exec alacritty' && sleep 1
-    swaymsg 'workspace 3; exec alacritty' && sleep 1
-    swaymsg 'workspace 4; exec alacritty' && sleep 1
-    swaymsg 'workspace 4; exec alacritty' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
     swaymsg 'workspace 9; exec keepassxc' && sleep 3
     swaymsg 'workspace 1'
 else
-    swaymsg 'workspace 1; exec alacritty' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
     swaymsg 'workspace 1; split v' && sleep 1
     swaymsg 'workspace 1; resize set height 3500px' && sleep 1
     swaymsg 'workspace 1; focus up;' && sleep 1
     swaymsg 'workspace 1; split h' && sleep 1
-    swaymsg 'workspace 1; exec alacritty' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
 
-    swaymsg 'workspace 2; exec alacritty' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
     swaymsg 'workspace 2; split v' && sleep 1
     swaymsg 'workspace 2; exec keepassxc' && sleep 1
     swaymsg 'workspace 2; focus up;' && sleep 1
@@ -38,17 +38,17 @@ else
     swaymsg 'workspace 2; focus up;' && sleep 1
     swaymsg 'workspace 2; focus up;' && sleep 1
     swaymsg 'workspace 2; split h' && sleep 1
-    swaymsg 'workspace 2; exec alacritty'
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
 
-    swaymsg 'workspace 8; exec alacritty' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
     swaymsg 'workspace 8; split v' && sleep 1
-    swaymsg 'workspace 8; exec alacritty -o font.size=12 --hold -e ssh cvoid -t "export TERM=rxvt; tmux a"' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
 
-    swaymsg 'workspace 3; exec alacritty' && sleep 1
-    swaymsg 'workspace 3; exec alacritty' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
 
-    swaymsg 'workspace 4; exec alacritty' && sleep 1
-    swaymsg 'workspace 4; exec alacritty' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
+    swaymsg 'workspace 3; exec $HOME/dotfiles/alacritty/alacritty-start.sh' && sleep 1
 
     swaymsg 'workspace 1' && sleep 1
     swaymsg 'workspace 2' && sleep 1
